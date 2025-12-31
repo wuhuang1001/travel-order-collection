34cfa9278ee5ab907342c8d2f9278005e068537e
wuhuang1001 on meow <779627546@qq.com>
2025-12-31 11:24:39 +0800

    临时多页解决方案

--- another_page.py
+++ another_page.py
@@ -1,4 +1,10 @@
 from main import main
 
 if __name__ == "__main__":
+    prompt1="别用202512-202512这种写法！！！"
+    prompt2="如果单月份请使用202512这种写法，如果多月份目前暂不支持！！！"
+    print(f"{prompt1:^20}")
+    print(prompt2)
+    div=''
+    print(f"{div:-^20}")
     main(pagenum=1)
